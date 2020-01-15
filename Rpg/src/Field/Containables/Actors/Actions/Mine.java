e2b7e8425dff226bfeb414110d7f41340c010be0
Azakira <47180416+Azakira@users.noreply.github.com>
2020-01-15 02:24:51 +0100

    removed sysous

--- Rpg/src/Field/Containables/Actors/Actions/Mine.java
+++ Rpg/src/Field/Containables/Actors/Actions/Mine.java
@@ -12,10 +12,8 @@ public class Mine extends Action{
 	public void mine() {
 		Wall w = this.chtr.getGround().getFloor().getNextWall(chtr.getGround(), chtr.getFacing());
 		if (w.isNull()) {
-			System.out.println("wall null");
 			return;
 		}
-		System.out.println(w.getCoordX()+", "+w.getCoordY());
 		this.chtr.getGround().getFloor().mineWall(w);
 	}
 
