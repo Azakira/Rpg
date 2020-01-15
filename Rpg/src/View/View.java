0c93ceff4661a43c5fbf3f014f83261a8ffde445
Azakira <47180416+Azakira@users.noreply.github.com>
2020-01-15 02:26:33 +0100

    new import + final consts WIDTH, HEIGHT added

--- Rpg/src/View/View.java
+++ Rpg/src/View/View.java
@@ -4,31 +4,31 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+import Field.Floor;
 import Model.Game;
 
 
 public class View {
- 
+	
+	public final int WIDTH = (ViewGame.size+3)*Floor.WIDTH;
+	public final int HEIGHT = (ViewGame.size+4)*Floor.HEIGHT;
+	//public final static String path = "D:\\Linux\\POGL\\Coltexpress\\src\\game\\Images\\";
+	
     private JFrame frame;
 
     private ViewGame vGame;
-    //private ViewCommands vCommands;
     
-    public final static String path = "D:\\Linux\\POGL\\Coltexpress\\src\\game\\Images\\";
+   
     public View(Game game) {
     	frame = new JFrame();
     	frame.setTitle("Rpg");
-		frame.setLayout(new BorderLayout());
+//		frame.setLayout(new BorderLayout());
+    	frame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		vGame = new ViewGame(game);
+		frame.add(vGame); //frame.add(vGame, BorderLayout.WEST);
 		
-		//commands = new ViewCommands(game);
-		
-		
-		frame.setSize(1425, 550);
-		frame.add(vGame, BorderLayout.WEST);
-		//frame.add(vCommands, BorderLayout.EAST);
-		
-		//frame.pack();
+		frame.pack();
+		frame.setVisible(true);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
