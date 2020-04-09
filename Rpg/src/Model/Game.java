ff65a8d7d4aa499dd2ad1e37571e7dcf7ce78018
Azakira <47180416+Azakira@users.noreply.github.com>
2020-04-09 19:15:30 +0200

    new attribute bool readyToDisplay + function wait

--- Rpg/src/Model/Game.java
+++ Rpg/src/Model/Game.java
@@ -1,44 +1,82 @@
 package Model;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import org.omg.CORBA.Current;
+
 import Field.Floor;
 import Field.Containables.Actors.Character;
 import Field.Containables.Actors.Hero;
 import Field.Containables.Actors.Actions.Direction;
 import Field.Containables.Actors.Actions.Move;
+import View.ViewGame;
+import View.View;
 
 public class Game {
 	
 	public ArrayList<Floor> floors;
-	private int currentFloor;
+	private int nbFloor;
 	public Hero hero;
+	public boolean readyToDisplay;
+	
+	public View view;
 	
 	public Game() {
+		this.readyToDisplay = false;
+		this.view = new View(this);
 		this.hero = new Hero("Kevin", 1, null);
 		this.floors = new ArrayList<Floor>(0);
-		this.currentFloor = 0;
-		this.floors.add(new Floor(this, currentFloor));
-		
+		this.nbFloor = 0;
+		this.floors.add(new Floor(this, nbFloor));
+		this.readyToDisplay = true;
 	}
 	
 	public static void main(String[] args) {
 		Game game = new Game();
+		wait(1);
 		game.hero.moveDown();
+		wait(1);
 		game.hero.moveDown();
+		wait(1);
 		game.hero.moveDown();
+		wait(1);
 		game.hero.moveDown();
+		wait(1);
 		game.hero.moveDown();
+		wait(1);
 		game.hero.mine();
+		wait(1);
 		game.hero.moveDown();
+		wait(1);
 		game.hero.mine();
+		wait(1);
 		game.hero.moveDown();
+		wait(1);
 		game.hero.mine();
+		wait(1);
 		game.hero.moveDown();
-		System.out.println(game.floors.get(0));
+		//System.out.println(game.floors.get(0));
 		
 	}
 	
-	public int getCurrentFloor() {
-		return currentFloor;
+	private static void wait(int sec) {
+		try {
+			TimeUnit.SECONDS.sleep(sec);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Floor getCurrentFloor() {
+		return this.floors.get(nbFloor);
+	}
+	
+	public ViewGame getViewG() {
+		return this.view.vGame;
+	}
+	
+	public void setView(View view) {
+		this.view = view;
 	}
 
 }
